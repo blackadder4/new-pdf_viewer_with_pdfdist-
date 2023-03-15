b4b110b7c238a14b920f296cea2b06a0f0d9a3b9
blackadder4 <leo.huang@bellevuecollege.edu>
2023-03-14 17:18:13 -0700

    first commit

---
+++ presentee_app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PdfUploader from './PdfUploader';
+
+function App() {
+  return (
+    <div className="App">
+      <PdfUploader />
+    </div>
+  );
+}
+
+export default App;
